20121f1ad8cf5f6ef85c38e4a97ceecec99bed62
Yury Samsonenko <cant09y@gmail.com>
2023-12-07 19:32:29 +0300

    feat: add tabs for menu

---
+++ coffee-house/assets/js/menu.js
@@ -0,0 +1,46 @@
+// Tabs
+
+window.addEventListener('DOMContentLoaded', () => {
+  const tabs = document.querySelectorAll('.tabs__tab');
+  const tabNames = document.querySelectorAll('.tabs__tab-name');
+
+  function removeSelectedTabs() {
+    tabs.forEach((tab) => {
+      tab.classList.remove('tabs__tab_select');
+    });
+  }
+
+  function selectClickedTab(clickedTag) {
+    clickedTag.classList.add('tabs__tab_select');
+  }
+
+  function filterBySelectedTab(selectTab) {
+    const products = document.querySelectorAll('.tabs__product');
+    products.forEach((product) => {
+      if (product.closest(`[data-category="${selectTab}"]`)) {
+        product.classList.remove('tabs__product_hidden');
+      } else {
+        product.classList.add('tabs__product_hidden');
+      }
+    });
+  }
+
+  filterBySelectedTab(tabNames[0].innerHTML);
+
+  document.querySelector('.tabs__flex-container').addEventListener('click', (e) => {
+    const target = e.target;
+    const selectTab = target
+      .closest('.tabs__tab')
+      .querySelector('.tabs__tab-name').innerHTML;
+
+    if (target.closest('.tabs__tab')) {
+      tabs.forEach((tab, i) => {
+        if (tab === target.closest('.tabs__tab')) {
+          removeSelectedTabs();
+          selectClickedTab(tabs[i]);
+          filterBySelectedTab(selectTab);
+        }
+      });
+    }
+  });
+});
