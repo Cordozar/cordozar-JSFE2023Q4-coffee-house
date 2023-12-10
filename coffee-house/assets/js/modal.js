4e2724bbe7b73a189a505450a7153fd26dfb44b8
Yury Samsonenko <cant09y@gmail.com>
2023-12-10 23:41:32 +0300

    feat: add video

--- coffee-house/assets/js/modal.js
+++ coffee-house/assets/js/modal.js
@@ -1,5 +1,76 @@
 window.addEventListener('DOMContentLoaded', () => {
   const modal = document.querySelector('.modal');
+  const products = document.querySelectorAll('.tabs__product');
+
+  function createModal(cards, numberCard) {
+    modal.innerHTML = '';
+    for (let i = 0; i < cards.length; i += 1) {
+      if (numberCard === i) {
+        modal.innerHTML += `<div class="modal__content">
+        <div class="modal__img"></div>
+        <div class="modal__information">
+          <div class="modal__title">
+            ${cards[i].name}
+          </div>
+          <p class="modal__description">
+            ${cards[i].description}
+          </p>
+          <span class="modal__choice">Size</span>
+          <div class="tabs__flex-container tabs__flex-container_modal">
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">S</div>
+              <div class="tabs__tab-name">${cards[i].sizes.s.size}</div>
+            </div>
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">M</div>
+              <div class="tabs__tab-name">${cards[i].sizes.m.size}</div>
+            </div>
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">L</div>
+              <div class="tabs__tab-name">${cards[i].sizes.l.size}</div>
+            </div>
+          </div>
+          <span class="modal__choice">Additives</span>
+          <div class="tabs__flex-container tabs__flex-container_modal">
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">1</div>
+              <div class="tabs__tab-name">${cards[i].additives[0].name}</div>
+            </div>
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">2</div>
+              <div class="tabs__tab-name">${cards[i].additives[1].name}</div>
+            </div>
+            <div class="tabs__tab">
+              <div class="tabs__tab-icon">3</div>
+              <div class="tabs__tab-name">${cards[i].additives[2].name}</div>
+            </div>
+          </div>
+          <div class="modal__total">
+            <span class="modal__text">Total:</span>
+            <span class="modal__price">$${cards[i].price}</span>
+          </div>
+          <p class="modal__sale">The cost is not final. Download our mobile app to see the final price and place your
+            order. Earn loyalty points and enjoy your favorite coffee with up to 20% discount.</p>
+          <div class="modal__close">Close</div>
+        </div>
+      </div>`;
+      }
+    }
+  }
+
+  let requestProducts;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', '../../assets/products.json', true);
+  xhr.send();
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      requestProducts = JSON.parse(xhr.responseText);
+    } else {
+      console.log('Error');
+    }
+  };
+
   const body = document.querySelector('body');
 
   function closeModal() {
@@ -29,6 +100,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
   productsParent.addEventListener('click', (e) => {
     if (e.target.closest('.tabs__product')) {
+      let index;
+      products.forEach((el, i) => {
+        if (el === e.target.closest('.tabs__product')) {
+          index = i;
+        }
+      });
+      createModal(requestProducts, index);
       openModal();
     }
   });
